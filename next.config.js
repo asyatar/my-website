1c45bb4882e5e10e7d88b0edb6bb3cd5b60e83ae
Asya Tarabar <asyatarabar@Asyas-MacBook-Air.local>
2024-12-31 20:17:14 -0500

    Add GitHub Actions workflow

---
+++ next.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  basePath: '/my-website',
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;
