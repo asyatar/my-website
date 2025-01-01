6e83021ed2f51baa721e98d00019e668465468ae
Asya Tarabar <asyatarabar@Asyas-MacBook-Air.local>
2024-12-31 20:23:01 -0500

    Update deployment config

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  basePath: '/my-website',
+  basePath: '/my-website', // Must match repo name exactly
   images: {
     unoptimized: true,
   },
